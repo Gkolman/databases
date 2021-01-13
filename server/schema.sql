ef6d942ae2a4092185b4c519c07c14cfa1631969
Gkolman <Gkolman17@gmail.com>
2021-01-12 20:20:39 -0800

    end of day pairing 1/12/21

--- server/schema.sql
+++ server/schema.sql
@@ -3,6 +3,33 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
+
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user` INTEGER NULL DEFAULT NULL,
+  `message` INTEGER NULL DEFAULT NULL,
+  `roomName ` INTEGER NULL DEFAULT NULL,
+  `timeCreated ` INTEGER NULL DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+
+  /* Describe your table here.*/
+);
+
+CREATE TABLE users (
+
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `User Names` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+
+  /* Describe your table here.*/
+);
+
+
+CREATE TABLE rooms (
+
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT 1,
+  `Room names ` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
   /* Describe your table here.*/
 );
 
