136139609feb5fb4d0aab056892430ced26315ab
Gkolman <Gkolman17@gmail.com>
2021-01-14 12:08:33 -0800

    end of pairing sesh

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,20 @@
 var db = require('../db');
-
+console.log('database -> ', db);
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      db.connect(function(err) {
+        if (err) { throw err; }
+        db.query( 'SELECT * FROM messages', function (err, result, fields) {
+          if (err) { throw err; }
+          console.log('result -> ',result);
+        });
+      });
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
-
+  // get retrieve  select * messages;
+  // post insert .. .value
   users: {
     // Ditto as above.
     get: function () {},
